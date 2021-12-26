2b43bf5d369119475fae5a665758932f87aa317a
minkaunghtet <minkaunghtet.greenway.com.org@gmail.com>
2021-12-26 18:48:13 +0630

    update naming

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -9,10 +9,13 @@ import UIKit
 
 class ViewController: UIViewController, UINavigationControllerDelegate {
 
+    @IBOutlet weak var buttonToolBar: UIToolbar!
     @IBOutlet weak var cameraButton: UIButton!
     @IBOutlet weak var imagePickerView: UIImageView!
-    @IBOutlet weak var TopText: UITextField!
-    @IBOutlet weak var ButtomText: UITextField!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var topNavBar: UIToolbar!
+    var memedImage: UIImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,21 +23,21 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
             NSAttributedString.Key.strokeColor: UIColor.black,
             NSAttributedString.Key.foregroundColor: UIColor.white,
             NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
-            NSAttributedString.Key.strokeWidth: 3.00
+            NSAttributedString.Key.strokeWidth: -3.00
         ]
-        TopText.defaultTextAttributes = memeTextAttributes
-        TopText.text = "Top"
-        TopText.textAlignment = .center
-        TopText.borderStyle = .none
-        TopText.textColor = UIColor.white
-        TopText.delegate = self
+        topTextField.defaultTextAttributes = memeTextAttributes
+        topTextField.text = "Top"
+        topTextField.textAlignment = .center
+        topTextField.borderStyle = .none
+        topTextField.textColor = UIColor.white
+        topTextField.delegate = self
       
-        ButtomText.defaultTextAttributes = memeTextAttributes
-        ButtomText.text = "Buttom"
-        ButtomText.textAlignment = .center
-        ButtomText.textColor = UIColor.white
-        ButtomText.delegate = self
-        ButtomText.borderStyle = .none
+        bottomTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.text = "Bottom"
+        bottomTextField.textAlignment = .center
+        bottomTextField.textColor = UIColor.white
+        bottomTextField.delegate = self
+        bottomTextField.borderStyle = .none
        
     }
 
@@ -57,7 +60,18 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
             present(imagePicker, animated: true, completion: nil)
     }
     
-    
+    @IBAction func shareItems(sender: AnyObject) {
+           
+           let memeToShare = generateMemedImage()
+           
+           let activityViewController = UIActivityViewController(activityItems: [memeToShare], applicationActivities: nil)
+           activityViewController.completionWithItemsHandler = { activity, success, items, error in
+               if success {
+                   self.safelySaveMeme(memedImage: memeToShare)
+               }
+           }
+        present(activityViewController, animated: true, completion: nil)
+       }
     
     
     @IBAction func pickAnImageFromCamera(_ sender: Any) {
@@ -89,7 +103,7 @@ extension ViewController: UIImagePickerControllerDelegate {
 extension ViewController: UITextFieldDelegate {
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
-        if textField == TopText {
+        if textField == TopTextField {
             textField.text  = ""
         }else {
             textField.text = "" //Butoom Text Field
@@ -121,18 +135,20 @@ extension ViewController {
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
-   func keyboardWillShow(notification: NSNotification) {
+   @objc func keyboardWillShow(notification: NSNotification) {
 
-        if ButtomText.isFirstResponder && view.frame.origin.y == 0.0{
-            view.frame.origin.y -= getKeyboardHeight(notification)
+        if bottomTextField.isFirstResponder && view.frame.origin.y == 0 {
+            view.frame.origin.y -= getKeyboardHeight(notification as Notification)
         }
     }
+
+    
     
     func keyboardWillHide(notification: NSNotification) {
-        if ButtomText.isFirstResponder {
+        if bottomTextField.isFirstResponder {
                view.frame.origin.y = 0
            }
-       }
+    }
     
    
 
@@ -149,4 +165,53 @@ extension ViewController {
     }
 }
 
+//save meme
+extension ViewController {
+    
+    
+    struct Meme {
+        var topTextField: String!
+        var bottomTextField: String!
+        var originalImage: UIImage!
+        var memedImage: UIImage!
+    }
+    
+    func generateMemedImage() -> UIImage {
+          
+        toolBarVisible(visible: false)
+          
+          UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+          memedImage = UIGraphicsGetImageFromCurrentImageContext()
+          UIGraphicsEndImageContext()
+          
+        toolBarVisible(visible: true)
+          
+          return memedImage
+      }
+      func safelySaveMeme(memedImage: UIImage) {
+          // safely unwrap optionals
+          if imagePickerView.image != nil && topTextField.text != nil && bottomTextField.text != nil
+          {
+              let top = self.topTextField.text!
+              let bottom = self.bottomTextField.text!
+              let image = self.imagePickerView.image!
+              
+              let meme = Meme(topTextField: top, bottomTextField: bottom, originalImage: image, memedImage: memedImage)
+              (UIApplication.sharedApplication.delegate as! AppDelegate).memes.append(meme)
+          }
+      }
+      
+      // MARK: toolbar functions
+      func toolBarVisible(visible: Bool){
+          if !visible {
+              topNavBar.isHidden = true    // removed self
+              buttonToolBar.isHidden = true // typo on var for toolbar // removed self
+          } else {
+              topNavBar.isHidden = false   // removed self
+              buttonToolBar.isHidden = false  // removed self
+          }
+      }
+      
+}
 
